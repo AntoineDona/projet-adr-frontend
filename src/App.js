74bbfaaef26aded461ef4be18c4a5d2006756c64
Antoine Do Nascimento <antoinedn@live.fr>
2022-01-23 02:02:31 +0100

    feat: tab infos object + tab remain on refresh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
+import React, { useState } from "react"
 import Header from "./Components/Header"
 import Caisse from "./Components/Caisse"
+import Commandes from "./Components/Commandes"
+
 
 function App() {
+  const tabs = [
+    {
+      "id": "caisse",
+      "type": "caisse",
+      "title": "Caisse"
+    },
+    {
+      "id": "enkouizine",
+      "type": "enkouizine",
+      "title": "En Couisine"
+    },
+    {
+      "id": "waiting",
+      "type": "waiting",
+      "title": "En attente"
+    },
+    {
+      "id": "archived",
+      "type": "archived",
+      "title": "Archivées"
+    }
+  ]
+
+  // console.log("tab in local storage: ", localStorage.getItem('tab'))
+  // console.log("value of tabs :", tabs.at(0))
+
+  if (localStorage.getItem('tab') === null) { 
+    localStorage.setItem('tab', JSON.stringify(tabs.at(0))) 
+  }
+
+  const [tab, setTab] = useState(JSON.parse(localStorage.getItem('tab')))
+  
+  let toRender = <Caisse tab={tab}/>
+  
+  if (tab.id === "caisse") {
+    toRender = <Caisse tab={tab} />
+  } else {
+    toRender = <Commandes tab={tab} />
+  }
+
   return (
-    <div>
-      <Caisse />
-    </div>
+    <>
+      <Header
+        tabs={tabs}
+        setTab={setTab}
+      />
+      {toRender}
+    </>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,24 @@
-export default function Header() {
-    return (
-      <nav>
-          <img src="" alt="" id="logo_musee" />
-          <ul>
-              <li>Caisse</li>
-              <li>Validation</li>
-              <li>Preparation</li>
-              <li>Ecran client</li>
-          </ul>
-      </nav>
-    );
-  }
+import { useState } from "react"
+import logo from '../img/logo_musee_b.png'
+
+export default function Header({ tabs, setTab }) {
+  const [loader, reload] = useState(1)
+  return (
+    <nav>
+      <img src={logo} alt="" id="logo_musee" />
+      <ul>
+        {tabs.map((tab, i) => {
+          return (
+            <li key={i}>
+              <button onClick={() => {
+                setTab(tab);
+                reload(-1 * loader);
+                localStorage.setItem('tab', JSON.stringify(tab));
+              }}>{tab.title}</button>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  );
+}
