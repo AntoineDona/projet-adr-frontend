53992d6bb28299d0abce1eb66795ba310541b6cc
Antoine Do Nascimento <antoinedn@live.fr>
2022-02-03 14:31:14 +0100

    feat: archived inverse diplay order

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -87,7 +87,7 @@ export default function Client() {
   }
 
   function commandList(articlestatus) {
-    let commands_array = false ? commands.slice(0).reverse() : commands;
+    let commands_array = articlestatus==="archived" ? commands.slice(0).reverse() : commands;
     return (
       commands_array.map((command) => {
         let mappingOfCommand = mapCommand(command, articlestatus);
@@ -98,7 +98,7 @@ export default function Client() {
             console.log("last update",command.last_update);
             console.log("display1? ", todisplay1, Date.parse(command.last_update)-new Date()+10*60*1000);
         }
-        if (mappingOfCommand.some(e => typeof (e) === 'object') && todisplay1 && todisplay2) { //si un objet est présent dans l'array, cad il y a un article
+        if (mappingOfCommand.some(e => typeof (e) === 'object') && ((todisplay1 && todisplay2 &&  articlestatus === "archived" ) || articlestatus === "enkouizine" )) { //si un objet est présent dans l'array, cad il y a un article
           return ( // on affiche le conteneur de la commande
             <div key={command._id} className="commande client">
               <div className="cmd_infos">
